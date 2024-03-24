262853e5aa34cb54cb25a6569589b3a0d643c75e
zaaynn123 <zaingamer99@gmail.com>
2024-03-25 04:03:58 +0500

    third commit

--- main/main.c
+++ main/main.c
@@ -15,6 +15,7 @@
 #include <sys/types.h>
 #include "esp_mac.h"
 #include "esp_wifi.h"
+#include "driver/ledc.h"
 #include "esp_event.h"
 #include "lwip/err.h"
 #include "lwip/sys.h"
@@ -23,10 +24,87 @@
 #define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
 #define EXAMPLE_ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
 #define EXAMPLE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          (5) // Define the output GPIO
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (4096) // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define LEDC_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
+
+static void example_ledc_init(int gpio_numb)
+{
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .timer_num        = LEDC_TIMER,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 4 kHz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL,
+        .timer_sel      = LEDC_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = gpio_numb,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+}
 
 static const char *TAG = "wifi softAP";
 int wifi_connect_status = 0;
 char instruction[10];
+int forward = 0;
+int reverse = 0;
+int left = 0;
+int right = 0;
+
+
+void Control( void * pvParameters )
+{
+    for( ;; )
+    {
+        if(strcmp(instruction, "forward") == 0){
+           if(forward){      
+           example_ledc_init(5);  
+           ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+           ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+           ESP_LOGI(TAG, "In Forward");}
+           else{
+           ESP_LOGI(TAG, "STOP(Forward)");
+        }
+        }
+        else if(strcmp(instruction, "reverse") == 0){
+            if(reverse){      
+            ESP_LOGI(TAG, "In Reverse");}
+            else{
+            ESP_LOGI(TAG, "STOP(Reverse)");
+        }
+        }
+        else if(strcmp(instruction, "left") == 0){
+            if(left){      
+            ESP_LOGI(TAG, "In Left");}
+            else{
+            ESP_LOGI(TAG, "STOP(Left)");
+        }
+        }
+        else if(strcmp(instruction, "right") == 0)
+        {
+            if(right){   
+            ESP_LOGI(TAG, "In Right");}
+            else{
+            ESP_LOGI(TAG, "STOP(Right)");
+        }
+        }
+        vTaskDelay(100 / portTICK_PERIOD_MS );
+    }
+}
 
 static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                     int32_t event_id, void* event_data)
@@ -226,6 +304,15 @@ static esp_err_t handle_ws_req(httpd_req_t *req)
             return ret;
         }
         ESP_LOGI(TAG, "Got packet with message: %s", ws_pkt.payload);
+        strcpy(instruction,(char *)ws_pkt.payload);
+        if(strcmp(instruction, "forward") == 0)
+        forward = !forward;
+        else if(strcmp(instruction, "reverse") == 0)
+        reverse = !reverse;
+        else if(strcmp(instruction, "left") == 0)
+        left = !left;
+        else if(strcmp(instruction, "right") == 0)
+        right = !right;
     }
 
     ESP_LOGI(TAG, "frame len is %d", ws_pkt.len);
@@ -279,6 +366,8 @@ void app_main()
 
     wifi_init_softap();
     wifi_connect_status = 1;
+
+    xTaskCreate(Control,"Control_Task",1024, NULL, 9, NULL );
 
     if (wifi_connect_status)
     {
